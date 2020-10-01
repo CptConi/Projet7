27e711280a2de431f7dabeab2b82d109ed5c2930
CptConi <nicolas.renard.33@gmail.com>
2020-10-01 15:33:26 +0200

    add auth guard to identity view

--- frontend/src/views/Identity.vue
+++ frontend/src/views/Identity.vue
@@ -7,15 +7,29 @@
 				</b-col>
 			</b-row>
 		</b-container>
-	<FirepitAnimated></FirepitAnimated>
+		<FirepitAnimated></FirepitAnimated>
 	</div>
 </template>
 
 <script>
-import FirepitAnimated from "../components/Firepit-Animated"
+import FirepitAnimated from "../components/Firepit-Animated";
 import Settings from "../components/Settings";
+import { mapState, mapActions } from "vuex";
+
 export default {
 	components: { Settings, FirepitAnimated },
+	computed: {
+		...mapState(["isAuth"]),
+	},
+	methods: {
+		...mapActions(["setErrorMessage"])
+	},
+	beforeMount() {
+		if (!this.isAuth) {
+			this.setErrorMessage("Vous ne pouvez pas accéder à la page de profil sans être authentifié");
+			this.$router.push({ name: "Authentification" });
+		} 
+	},
 };
 </script>
 
